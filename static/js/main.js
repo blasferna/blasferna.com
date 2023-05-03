ca99a3cfc64d61df9232c7dfd736b7c6207a2734
blasferna <blasferna@users.noreply.github.com>
2023-05-03 00:01:01 +0000

    deploy: 248a421d5096bcaaf9e531e7bc3cc41b9bf4cb6d

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,75 @@
 /**
  * $(document).ready() in pure javascript.
- * @param {*} fn 
+ * @param {*} fn
  */
 function ready(fn) {
-    if (document.readyState !== 'loading') {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
+  if (document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
 
+ready(function () {
+  const openMenu = document.getElementById("openMenu");
+  const closeMenu = document.getElementById("closeMenu");
+  const hiddenMenu = document.getElementById("hiddenMenu");
 
-ready(function(){
-    const openMenu = document.getElementById("openMenu");
-    const closeMenu = document.getElementById("closeMenu");
-    const hiddenMenu = document.getElementById("hiddenMenu");
+  const languageButton = document.getElementById("languageButton");
+  const languageList = document.getElementById("languageList");
+  const languageItems = document.querySelectorAll('[role="menuitem"]');
 
-    const toggleMenu = function(){
-        console.log("aquiiii")
-        let isOpen = hiddenMenu.classList.contains("translate-x-0");
+  const toggleMenu = function () {
+    let isOpen = hiddenMenu.classList.contains("translate-x-0");
 
-        if (isOpen){
-            hiddenMenu.classList.add("translate-x-full")
-            hiddenMenu.classList.remove("translate-x-0")
-        }else{
-            hiddenMenu.classList.add("translate-x-0")
-            hiddenMenu.classList.remove("translate-x-full")            
-        }
+    if (isOpen) {
+      hiddenMenu.classList.add("translate-x-full");
+      hiddenMenu.classList.remove("translate-x-0");
+    } else {
+      hiddenMenu.classList.add("translate-x-0");
+      hiddenMenu.classList.remove("translate-x-full");
     }
+  };
 
-    openMenu.addEventListener("click", toggleMenu);
-    closeMenu.addEventListener("click", toggleMenu);
+  openMenu.addEventListener("click", toggleMenu);
+  closeMenu.addEventListener("click", toggleMenu);
+
+  languageButton.addEventListener("click", () => {
+    languageList.classList.toggle("hidden");
+  });
+
+  languageItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      const lang = item.getAttribute("data-lang");
+      handleLanguageSelection(lang);
+      languageList.classList.add("hidden");
+      languageButton.querySelector("span").textContent = item.textContent;
+    });
+  });
+
+  function handleLanguageSelection(lang) {
+    localStorage.setItem("selectedLanguage", lang);
+    window.location.href = `/${lang}/`
+  }
+
+  const storedLanguage = localStorage.getItem("selectedLanguage");
+  var selectedLanguage = null;
+
+  if (storedLanguage) {
+    selectedLanguage = storedLanguage;
+  } else {
+    const htmlLang = document.documentElement.getAttribute("lang");
+    localStorage.setItem("selectedLanguage", htmlLang);
+    selectedLanguage = htmlLang;
+  }
+
+  const storedLanguageItem = document.querySelector(
+    `[data-lang=${selectedLanguage}]`
+  );
+  console.log(storedLanguageItem);
+  if (storedLanguageItem) {
+    languageButton.querySelector("span").textContent =
+      storedLanguageItem.textContent;
+  }
 });
